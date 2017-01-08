e2479cfea8c49d5987624f784d36023223ab2165
Belbertn <belbert95@me.com>
2017-01-08 02:23:24 +0100

    zv

--- Shoot/Shoot/MainGame.cs
+++ Shoot/Shoot/MainGame.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -9,12 +10,17 @@ namespace Shoot
         public ContentManager Content { get; private set; }
         public SpriteBatch Batch { get; private set; }
 
-        public IEntity entities { get; private set; }
+        public List<IEntity> entities { get; private set; }
 
         public void Load(ContentManager content, SpriteBatch spriteBatch)
         {
             Content = content;
             Batch = spriteBatch;
+
+            foreach(IEntity entity in entities)
+            {
+                entity.Load(Content);
+            }
         }
 
         public void UnLoad()
@@ -24,13 +30,19 @@ namespace Shoot
 
         public void Update(GameTime gameTime)
         {
-
+            foreach(IEntity entity in entities)
+            {
+                entity.Update(gameTime);
+            }
         }
 
         public void Draw()
         {
             Batch.Begin();
-
+            foreach(IEntity entity in entities)
+            {
+                entity.Draw(Batch);
+            }
             Batch.End();
         }
     }
