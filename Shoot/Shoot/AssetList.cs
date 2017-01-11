b74b8d7cd7fae1dd02351152af457a9d84bfec16
Belbertn <belbert95@me.com>
2017-01-11 19:23:54 +0100

    AssetList is a directory of all the files that contain lists of asset names. IE textures

---
+++ Shoot/Shoot/AssetList.cs
@@ -0,0 +1,17 @@
+namespace Shoot
+{
+    public struct AssetList
+    {
+        public static string Level1 
+        {
+            get
+            {
+                return "Level1_Texture_List.txt";
+            }
+            set
+            {
+                throw new System.FieldAccessException();
+            }
+        }
+    }
+}
